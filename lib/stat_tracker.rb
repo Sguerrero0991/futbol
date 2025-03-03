702a6692c6ce392ba35f2dd42efb8870f918ac9d
Brady Blair <bblair321@yahoo.comm>
2025-03-03 10:01:36 -0800

    Added a class method for my tests to be able to load the csv data

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,11 @@
+require 'CSV'
 class StatTracker 
   def initialize
+
+  end
+
+  def self.from_csv(file_path)
+    # Open and read the CSV file, then return the data (this can be an array of rows)
+    CSV.read(file_path, headers: true)
   end
 end
