29da3b1ebd5337953319bab2a77bc575521908e4
Brady Blair <bblair321@yahoo.comm>
2025-03-03 12:19:06 -0800

    Changed the class method in out stat tracker.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,8 +4,8 @@ class StatTracker
 
   end
 
-  def self.from_csv(file_path)
+  def self.from_csv(locations)
     # Open and read the CSV file, then return the data (this can be an array of rows)
-    CSV.read(file_path, headers: true)
+    CSV.read(locations, headers: true)
   end
 end
