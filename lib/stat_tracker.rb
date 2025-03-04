043ff7566e7b13cdf54e8284f7151e590c6828ca
Brady Blair <bblair321@yahoo.comm>
2025-03-03 17:10:06 -0800

    We got our stat_tracker class setup and working

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,11 +1,23 @@
 require 'CSV'
-class StatTracker 
-  def initialize
-
-  end
-
+class StatTracker
   def self.from_csv(locations)
-    # Open and read the CSV file, then return the data (this can be an array of rows)
-    CSV.read(locations, headers: true)
+    # Read each CSV file using the correct paths
+    games = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+    teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+    # Print out the counts of the loaded data
+    puts "Games loaded: #{games.count}"
+    puts "Teams loaded: #{teams.count}"
+    puts "GameTeams loaded: #{game_teams.count}"
+    # Create a new instance of StatTracker
+    stat_tracker = StatTracker.new(games, teams, game_teams)
+    puts "StatTracker instance: #{stat_tracker.inspect}"
+    # Return the instance
+    return stat_tracker
+  end
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 end
