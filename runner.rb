d57f35568b1adf733fc8f21f4409f407a8c05034
Brady Blair <bblair321@yahoo.comm>
2025-03-03 17:10:36 -0800

    Reworked our runner.rb file

--- runner.rb
+++ runner.rb
@@ -2,35 +2,6 @@ require_relative './lib/stat_tracker'
 require 'pry'
 require 'CSV'
 
-# CSV.foreach('./data/games.txt', headers: true, header_converters: :symbol) do |row|
-#    row[0]    # Access column by index
-#    season = row[:season].to_i
-#    game_id = row[:game_id] # Access column by header name
-#    type = row[:type]
-#    date_time = row[:date_time]
-#    away_team_id = row[:away_team_id]
-#    home_team_id = row[:home_team_id]
-#    away_goals = row[:away_goals]
-#    home_goals = row[:home_goals]
-#    venue = row[:venue]
-#    venue_link = row[:venue_link]
-# end 
-
-CSV.foreach('./data/teams.txt', headers: true, header_converters: :symbol) do |row|
-  team_id = row[:team_id]
-  franchiseid = row[:franchiseid]
-  teamName = row[:teamName]
-  abbreviation = row[:abbreviation]
-  stadium = row[:stadium]
-  link = row[:link]
-
-end
-
-# CSV.foreach('./data/game_teams.csv', headers: true, header_converters: :symbol) do |row|
-#   puts row['Name'] # Access column by header name
-#   puts row[0]    # Access column by index
-# end 
-
 game_path = './data/games.txt'
 team_path = './data/teams.txt'
 game_teams_path = './data/game_teams.txt'
@@ -40,7 +11,5 @@ locations = {
   teams: team_path,
   game_teams: game_teams_path
 }
-binding.pry
+# binding.pry
 stat_tracker = StatTracker.from_csv(locations)
-
-
