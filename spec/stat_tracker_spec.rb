70cba1d66d775beebddf77984c6ffc0ce133d28c
Brady Blair <bblair321@yahoo.comm>
2025-03-05 14:39:31 -0800

    Fixed our stat tracker spec file. I did some reasearch and found that we needed to use before:each instead of before:all. Switched the variables to instnace variables. I also realized we weren't requiring the ruby csv. So added that as well. Everything is now passing

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,50 +1,48 @@
 require '../futbol/lib/stat_tracker'
+require 'csv'
 require 'pry'
 
 RSpec.describe StatTracker do
-  before(:all) do
-    games_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+  before(:each) do
+    # Convert local variables to instance variables
+    @games_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
 
-    locations = {
-      games: games_path,
-      teams: team_path,
-      game_teams: game_teams_path
+    @locations = {
+      games: @games_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
     }
 
-    @stat_tracker = StatTracker.from_csv(locations)
+    @stat_tracker = StatTracker.from_csv(@locations)
   end
 
   it 'exists' do
-    expect(@stat_tracker).to be_an_instance_of StatTracker
+    expect(@stat_tracker).to be_an_instance_of(StatTracker)
   end
 
   it 'stores a file path of games in a hash' do
-    expect(@stat_tracker.games).to eq('./data/games.csv')
+    expect(@stat_tracker.games).to eq(@games_path)
   end 
- 
 
   it 'stores a file path of teams in a hash' do
-    expect(@stat_tracker.teams).to eq('./data/teams.csv')
+    expect(@stat_tracker.teams).to eq(@team_path)
   end 
 
   it 'stores a file path of game_teams in a hash' do
-    expect(@stat_tracker.game_teams).to eq('./data/game_teams.csv')
+    expect(@stat_tracker.game_teams).to eq(@game_teams_path)
   end 
 
-  describe '.from_csv' do
+  describe 'from_csv' do
     it 'reads the correct CSV files' do
+      # Now @locations and @games_path are properly set in before(:each)
       stat_tracker = StatTracker.from_csv(@locations)
 
-      # Read the test CSV file manually to compare
       games = CSV.read(@games_path, headers: true, header_converters: :symbol)
 
-      # Check if the CSV was read correctly
       expect(games).not_to be_empty
-      expect(games.headers).to include(:game_id, :season, :home_team_id) # Adjust based on actual headers
-      
+      expect(games.headers).to include(:game_id, :season, :home_team_id)
     end
   end
-  binding.pry
-end 
+end
