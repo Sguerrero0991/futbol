d170c07ecd2e4f8d10f2c782c27b37e9cfec4596
Brady Blair <bblair321@yahoo.comm>
2025-03-06 09:58:49 -0800

    we have to create objects with the csv data

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,8 +12,9 @@ class StatTracker
  
   def self.from_csv(locations)
     # Read each CSV file using the correct paths
-    games = CSV.read(locations[:games], headers: true, header_converters: :symbol).map(&:to_h)
-    teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol).map { |row| Teams.new(row) }    game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol).map(&:to_h)
+    games = CSV.read(locations[:games], headers: true, header_converters: :symbol).map { |row| Games.new(row) }
+    teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol).map { |row| Teams.new(row) }    
+    game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol) { |row| Games_Teams.new(row) }
     # Create a new instance of StatTracker
     new(games, teams, game_teams)
   end
