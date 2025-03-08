0523d30f0383140f10924753827047ab5bd1cdd7
Calvin Suiter <cdsuiter@yahoo.com>
2025-03-08 14:12:39 -0500

    completed code for assigned methods, added attribute methods to games, teams, and game_teams to allow for use of hash notation, updated hoa and team_name attributes

--- lib/teams.rb
+++ lib/teams.rb
@@ -1,15 +1,44 @@
 require 'csv'
 require 'pry'
 class Teams
-  attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
+  attr_reader :team_id, 
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link
+
   def initialize(team_data)
-    @team_id = team_data[:team_id]
-    
-    @franchise_id = team_data[:franchiseid]
+    @team_id = team_data[:team_id].to_i
+    @franchise_id = team_data[:franchiseid].to_i
     @team_name = team_data[:teamname]
     @abbreviation = team_data[:abbreviation]
     @stadium = team_data[:stadium]
     @link = team_data[:link]
+  end
+
+  def team_id
+    @team_id
+  end
+
+  def franchise_id
+    @franchise_id
+  end
+
+  def team_name
+    @team_name
+  end
+
+  def abbreviation
+    @abbreviation
+  end
+
+  def stadium
+    @stadium
+  end
+
+  def link
+    @link
   end
 end
 
--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 require_relative './lib/stat_tracker'
 require './lib/games'
 require 'pry'
-require 'CSV'
+require 'csv'
 
 games_path = './data/games.csv'
 team_path = './data/teams.csv'
@@ -14,10 +14,14 @@ locations = {
 }
 
 stat_tracker = StatTracker.from_csv(locations)
-# binding.pry
+
 puts stat_tracker.highest_total_score
 puts stat_tracker.lowest_total_score
 puts stat_tracker.percentage_home_wins
 puts stat_tracker.percentage_visitor_wins
 puts stat_tracker.percentage_ties
-puts stat_tracker.count_of_games_by_season
+# puts stat_tracker.count_of_games_by_season
+puts stat_tracker.highest_scoring_visitor
+puts stat_tracker.highest_scoring_home_team
+puts stat_tracker.lowest_scoring_visitor
+puts stat_tracker.lowest_scoring_home_team
